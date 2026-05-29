548a79068d862b35e24035fb444628990bfa5b94
Rebal Daoud <dawoodrebal@gmail.com>
2026-05-29 15:44:55 +0300

    Update TrainPath.java

--- src/TrainPath.java
+++ src/TrainPath.java
@@ -1,3 +1,18 @@
+package Backend;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TrainPath {
+   private Map<Station, List<Path>> Network;
+
+   
+    public TrainPath() {
+        this.Network = new HashMap<>();
+    }
+
+    
 
 }
